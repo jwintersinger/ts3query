0b11b04b78f1c28bd2b113a7337a483c6732340d
Jeff Wintersinger <jeff.git@wintersinger.com>
2009-12-21 05:03:23 +0000

    Add JSON-formatted list of players to Web app.

--- ts3web.py
+++ ts3web.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import json
 import web
 from ts3query import TsConverser, Config
 
@@ -9,24 +10,51 @@ VOICE_PORT = Config.DEFAULT_VOICE_PORT
 
 # web.py miscellany.
 urls = (
-  '/', 'ChannelAndClientList'
+  '/',                        'ChannelAndClientList',
+  '/channels.json',           'ChannelAndClientListJson',
+  '/populated_channels.json', 'PopulatedChannelAndClientListJson'
 )
 render = web.template.render('templates/')
 app = web.application(urls, globals())
 
 
-class ChannelAndClientList:
+class BaseRequest:
+  def __init__(self):
+    self._tsc = TsConverser(ADDRESS, VOICE_PORT)
+
+
+class JsonRequest(BaseRequest):
+  def __init__(self):
+    BaseRequest.__init__(self)
+    web.header('Content-Type', 'application/json')
+
+
+class ChannelAndClientList(BaseRequest):
   '''Displays populated channels, as well as the clients within each.'''
 
   def GET(self):
-    tsc = TsConverser(ADDRESS, VOICE_PORT)
-    return render.channel_and_client_list(self._generate_server_name(), tsc.list_populated_channels())
+    return render.channel_and_client_list(self._generate_server_name(),
+      self._tsc.list_populated_channels())
 
   def _generate_server_name(self):
     server_name = ADDRESS
     if VOICE_PORT != Config.DEFAULT_VOICE_PORT:
       server_name = '%s:%s' % (server_name, VOICE_PORT)
     return server_name
+
+
+class ChannelAndClientListJson(JsonRequest):
+  '''Displays JSON-formatted list of all channels, as well as clients within each.'''
+
+  def GET(self):
+    return json.dumps(self._tsc.list_channels())
+
+
+class PopulatedChannelAndClientListJson(JsonRequest):
+  '''Displays JSON-formatted list of populated channels, as well as clients within each.'''
+
+  def GET(self):
+    return json.dumps(self._tsc.list_populated_channels())
 
 
 if __name__ == '__main__':
