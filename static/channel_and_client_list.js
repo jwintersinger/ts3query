9d8d1a78aede1c5dd9430664a4e75113672c122d
Jeff Wintersinger <jeff.git@wintersinger.com>
2010-01-03 01:17:34 +0000

    Reorganize mockup in preparation for integration with production.

--- static/channel_and_client_list.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.client-details').hide();
-  $('.client').mouseenter(function() {
-    $(this).find('.client-details').show();
-  }).mouseleave(function() {
-    $(this).find('.client-details').hide();
-  });
-});
